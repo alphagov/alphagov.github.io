e05ae2da5bbbccf7822a5c4d981936f5e5f07c83
Matthew Wall <mwall@alphagov.co.uk>
2012-04-30 15:20:46 +0100

    Re-instated design folder in subfolder called "design" Ensured that auto-regen script for maven repository leaves the design directory alone

--- update-directory-index.sh
+++ update-directory-index.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-for DIR in $(find . -type d  \( ! -regex '.*/\..*' \) ); do
+for DIR in $(find . -type d  \( ! -regex '.*/\..*' \) | grep -v design ); do
   echo -e "Creating directory listing index.html for "$DIR
   (
     echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
