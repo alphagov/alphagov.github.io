1f900f6bb269064ba59a0473f33ade7a40e08883
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2012-05-03 15:15:15 +0100

    Updating so that directory indexes appear automatically outside of the maven repo unless a directory index has already been created

--- update-directory-index.sh
+++ update-directory-index.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
- 
-for DIR in $(find maven -type d  \( ! -regex '.*/\..*' \) | grep -v design ); do
-  echo -e "Creating directory listing index.html for "$DIR
+
+function generate_directory_index {
+  echo -e "Creating directory listing index.html for "$1
   (
     echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
-    ls -1pa "${DIR}" |\
+    ls -1pa "${1}" |\
       grep -v "^\./$" |\
       grep -v "^\..*/$" |\
       grep -v "^index\.html$"|\
       awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
     echo -e "</pre>\n</body>\n</html>"
-  ) > "${DIR}/index.html"
+  ) > "${1}/index.html"
+}
+ 
+for DIR in $(find maven -type d  \( ! -regex '.*/\..*' \)); do
+ generate_directory_index $DIR
 done
+
+for DIR in $(find . -type d \( ! -regex '.*/\..*' \)); do
+	if([ ! -f ${DIR}/index.html ]);
+		then
+			generate_directory_index $DIR
+	fi
+done
+
