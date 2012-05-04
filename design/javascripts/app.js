c8f2a4170f9e22a7a0d33929a2840cfc9c9acc03
Guy Moorhouse <guy@futurefabric.co.uk>
2012-05-04 14:36:03 +0100

    Added click to copy to clipboard functionality to the colour palette
    Copies hex value to clipboard when clicking a colour block

--- design/javascripts/app.js
+++ design/javascripts/app.js
@@ -6,5 +6,23 @@ jQuery(document).ready(function ($) {
   		var rgb = $(this).css("background-color");
   		$(this).append("<p>" + rgb + "</p>");
 	});
-  
+
+    //copy hex to clipboard on click (desktop machines only, requires flash)
+    $('li').each(function(index) {
+    	$(this).zclip({
+	        path:'../javascripts/ZeroClipboard.swf',
+	        copy:$(this).children().filter("p:first").text(),
+	        
+	        beforeCopy:function(){
+            	$(this).css('opacity','0.5');
+	        },
+	        
+	        afterCopy:function(){
+	            $(this).css('opacity','1');
+	            $(".clipboard").text("Copied— " + $(this).children().filter("p:first").text());
+	        }
+        
+	    });
+
+	});
 });
