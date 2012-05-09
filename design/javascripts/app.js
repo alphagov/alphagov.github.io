0850d1c5ac64d050f66c84212d6eb55d7baacf90
Guy Moorhouse <guy@futurefabric.co.uk>
2012-05-09 17:28:35 +0100

    Progressively enhanced copy to clipboard feature
    Only shows copy to clipboard instruction when users have Flash 10 and
    up installed and it's not being blocked.
    
    (Required a hack using a small swf file).

--- design/javascripts/app.js
+++ design/javascripts/app.js
@@ -2,24 +2,23 @@
 jQuery(document).ready(function ($) {
 	
 	// Append RGB value after Hex value
+	
 	$("ul.palette li").each(function () {
   		var rgb = $(this).css("background-color");
   		$(this).append("<p>" + rgb + "</p>");
 	});
-
-    // Add copy to clipboard feature if Flash installedand above/equal to version 10
-	if(FlashDetect.installed && (FlashDetect.major >= 10)){
-	
-		//activateClickToCopy();
 		
-	}
-	
 });
 
 
 function activateClickToCopy() {
 	
+	// CLICK TO COPY HEX VALUE TO CLIPBOARD (REQUIRES FLASH 10 AND UP)
+	
 	// Use zclip to add SWFs to each palette li for the copying feature
+	// This function is called from ../javascripts/flash.block.detect.swf
+	// so we only get clipboard copy when people have flash version 10 and up and its not blocked
+	
 	$('li').each(function(index) {	    	
     	$(this).zclip({
         path:'../javascripts/ZeroClipboard.swf',
@@ -40,6 +39,5 @@ function activateClickToCopy() {
 	
 	// reveal the clipboard instruction/feedback notice
 	$(".clipboard").show();
-	
 }
 
